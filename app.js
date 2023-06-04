5a514f94907aecfcd29ebc78a429fa883206543a
hibatillah <hibatillah21si@mahasiswa.pcr.ac.id>
2023-06-04 20:40:54 +0700

    add disable lantai button for gedung-serbaguna some update

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
+// todo: add card status
 const cardStatus = document.querySelectorAll('.card-status')
 
 cardStatus.forEach((item) => {
   const status = item.getAttribute('class').split(' ')[1]
-  console.log("🚀 ~ file: app.js:3 ~ status:", status)
+  console.log(status)
 
   const statusText = ['Available', 'Unavailable', 'Booking', 'Done']
   const result = statusText.find((item) => item.toLowerCase() === status)
-  console.log("🚀 ~ file: app.js:9 ~ cardStatus.forEach ~ result:", result)
+  console.log(result)
   
   item.textContent = result;
 })
 
+// todo: disable lantai 3 button for 'gedung-serbaguna'
+const gedungRadio = document.querySelectorAll('input[name="gedung"]')
+const lantai3Radio = document.querySelector('label[for="lantai-3"]')
+let selected
+
+gedungRadio.forEach(item => {
+  item.addEventListener('click', (el) => {
+    selected = item.getAttribute("id"); 
+    console.log(selected)
+
+    if (selected === 'gedung-serbaguna') {
+      lantai3Radio.previousElementSibling.setAttribute('disabled',true)
+      console.log('disabled')
+      lantai3Radio.classList.add('disabled')
+    } else {
+      lantai3Radio.previousElementSibling.setAttribute('disabled',false)
+      console.log('enabled')
+      lantai3Radio.classList.remove("disabled");
+    }
+  })
+})
 
